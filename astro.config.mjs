9f1409a3d006741c62fa63a2593db9a6b8ce1a2b
MadLord <Anton.Tekutow@yandex.ru>
2025-09-21 07:46:06 +0300

    Paths fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from 'node:url';
+import { dirname, resolve } from 'node:path';
+
+const rootDir = dirname(fileURLToPath(import.meta.url));
+const pathFromRoot = (relativePath) => resolve(rootDir, relativePath);
 
 export default defineConfig({
   site: 'https://flexagon-technologies.github.io',
@@ -10,10 +15,10 @@ export default defineConfig({
   vite: {
     resolve: {
       alias: {
-        '@components': './src/components',
-        '@layouts': './src/layouts',
-        '@content': './src/content',
-        '@assets': './src/assets'
+        '@components': pathFromRoot('./src/components'),
+        '@layouts': pathFromRoot('./src/layouts'),
+        '@content': pathFromRoot('./src/content'),
+        '@assets': pathFromRoot('./src/assets')
       }
     }
   }
